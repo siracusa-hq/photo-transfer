ca8ca1ea45c23983e54bcc61b4846ef1b9dd6889
Hikaru Shimizu - siracusa <hshimizu@siracusa.jp>
2025-04-01 21:00:05 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,23 +2,23 @@ const CACHE_NAME = 'photo-uploader-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json',
-  // 必要に応じて、CSSやアイコンファイルなども追加
+  '/manifest.json'
+  // 必要に応じてCSSやアイコンも追加
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
+      .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then(response => response || fetch(event.request))
   );
 });
